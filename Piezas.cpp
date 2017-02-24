93fb70f6542435ab9e3e3a176af4d3e6b28ff93a
student <student@softeng>
2017-02-24 08:02:47 -0800

    Adding the Piezas.ccp functions code and some tests

--- Piezas.cpp
+++ Piezas.cpp
@@ -20,13 +20,26 @@
  * Constructor sets an empty board (default 3 rows, 4 columns) and 
  * specifies it is X's turn first
 **/
-Piezas::Piezas();
-
+Piezas::Piezas(){
+	board.resize(BOARD_ROWS);
+	for(unsigned int i = 0; i < board.size(); i++){
+	  board[i].resize(BOARD_COLS);
+	}
+	reset();
+	turn = X;
+}
+	
 /**
  * Resets each board location to the Blank Piece value, with a board of the
  * same size as previously specified
 **/
-void Piezas::reset();
+void Piezas::reset(){
+	for(int i = 0; i < BOARD_ROWS; i++){
+	  for(int j = 0; j < BOARD_COLS; j++){
+	    board[i][j] = Blank;
+	  }
+	}
+}
 
 /**
  * Places a piece of the current turn on the board, returns what
@@ -36,14 +49,49 @@ void Piezas::reset();
  * Out of bounds coordinates return the Piece Invalid value
  * Trying to drop a piece where it cannot be placed loses the player's turn
 **/ 
-Piece Piezas::dropPiece(int column);
-
+Piece Piezas::dropPiece(int column){
+	if(column > BOARD_COLS-1 || column < 0){
+	  if(turn == X){
+	    turn = O;
+	  }
+	  else{
+	    turn = X;
+	  }
+	  return Invalid;
+	}
+	if(pieceAt(2, column) != Blank){
+	  if(turn == X){
+	    turn = O;
+	  }
+	  else{
+	    turn = X;
+	  }
+	  return Blank;
+	}
+	Piece prevPiece = turn;
+	for(int i = 0; i < BOARD_ROWS; i++){
+	  if(board[i][column] == Blank){
+	    board[i][column] = turn;
+	    if(turn == X){
+	      turn = O;
+	    }
+	    else{
+	      turn = X;
+	    }
+	    return prevPiece;
+	  }
+	}
+}
 /**
  * Returns what piece is at the provided coordinates, or Blank if there
  * are no pieces there, or Invalid if the coordinates are out of bounds
 **/
-Piece Piezas::pieceAt(int row, int column);
-
+Piece Piezas::pieceAt(int row, int column){
+	if(row < 0 || row > BOARD_ROWS-1 || column < 0 || column > BOARD_COLS-1){
+	  return Invalid;
+	}
+	return board[row][column];
+}
 /**
  * Returns which Piece has won, if there is a winner, Invalid if the game
  * is not over, or Blank if the board is filled and no one has won ("tie").
@@ -53,4 +101,69 @@ Piece Piezas::pieceAt(int row, int column);
  * or horizontally. If both X's and O's have the same max number of pieces in a
  * line, it is a tie.
 **/
-Piece Piezas::gameState();
+Piece Piezas::gameState(){
+	int sumX = 1;
+	int sumO = 1;
+	int maxX = 0;
+	int maxO = 0;
+	int sumCX = 1;
+	int sumCO = 1;
+	int maxCX;
+	int maxCO;
+	for(int i = 0; i < BOARD_ROWS; i++){
+	  for(int j = 0; i < BOARD_COLS; j++){
+	    if(board[i][j] == Blank){
+	      return Invalid;
+	    }
+	  }
+	}
+	for(int i = 0; i < BOARD_ROWS; i++){
+	  for(int j = 0; j < BOARD_COLS-1; j++){
+	    if(board[i][j] == board[i][j+1]){
+	      if(board[i][j] == 'X'){
+		sumX++;
+	      }
+	      else{
+		sumO++;
+	      }
+	    }
+	  }
+	  if(sumX > maxX){
+	    maxX = sumX;
+	  }
+	  if(sumO > maxO){
+	    maxO = sumO;
+	  }
+	}
+	for(int i = 0; i < BOARD_COLS; i++){
+	  for(int j = 0; j < BOARD_ROWS-1; j++){
+	    if(board[i][j] == board[i+1][j]){
+	      if(board[i][j] == 'X'){
+		sumCX++;
+	      }
+	      else{
+		sumCO++;
+	      }
+	    }
+	  }
+	  if(sumCX > maxCX){
+	    maxCX = sumCX;
+	  }
+	  if(sumCO > maxCO){
+	    maxCO = sumCO;
+	  }
+	}
+	if(maxCX > maxX){
+	  maxX = maxCX;
+	}
+	if(maxCO > maxO){
+	  maxO = maxCO;
+	}
+	if(maxX > maxO){
+	  return X;
+	}
+	else if(maxO > maxX){
+	  return O;
+	}
+	return Blank;
+}
--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -18,3 +18,137 @@ TEST(PiezasTest, sanityCheck)
 {
 	ASSERT_TRUE(true);
 }
+
+TEST(PiezasTest, dropAtSmallerX){
+	Piezas board;
+	Piece actual;
+	actual = board.dropPiece(-1);
+	ASSERT_EQ(Invalid, actual);
+}
+TEST(PiezasTest, dropAtLargerX){
+	Piezas board;
+	Piece actual;
+	actual = board.dropPiece(4);
+	ASSERT_EQ(Invalid, actual);
+}
+TEST(PiezasTest, dropAtSmallerO){
+	Piezas board;
+	Piece actual = board.dropPiece(0);
+	actual = board.dropPiece(-1);
+	ASSERT_EQ(Invalid, actual);
+}
+TEST(PiezasTest, dropAtLargerO){
+	Piezas board;
+	Piece actual = board.dropPiece(0);
+	actual = board.dropPiece(4);
+	ASSERT_EQ(Invalid, actual);
+
+}
+TEST(PiezasTest, pieceAtOutLeft){
+	Piezas board;
+	Piece actual = board.pieceAt(0, -1);
+	ASSERT_EQ(Invalid, actual);
+}
+TEST(PiezasTest, pieceAtOutUp){
+	Piezas board;
+	Piece actual = board.pieceAt(-1, 0);
+	ASSERT_EQ(Invalid, actual);
+}
+TEST(PiezasTest, pieceAtOutDiagonal){
+	Piezas board;
+	Piece actual = board.pieceAt(-1, -1);
+	ASSERT_EQ(Invalid, actual);
+}
+TEST(PiezasTest, pieceAtOutRight){
+	Piezas board;
+	Piece actual = board.pieceAt(0, 4);
+	ASSERT_EQ(Invalid, actual);
+}
+TEST(PiezasTest, pieceAtOutRTop){
+	Piezas board;
+	Piece actual = board.pieceAt(-1, 3);
+	ASSERT_EQ(Invalid, actual);
+}
+TEST(PiezasTest, pieceAtOutRDiagonal){
+	Piezas board;
+	Piece actual = board.pieceAt(-1, 4);
+	ASSERT_EQ(Invalid, actual);
+}
+TEST(PiezasTest, pieceAtOutLTDiagonal){
+	Piezas board;
+	Piece actual = board.pieceAt(3, -1);
+	ASSERT_EQ(Invalid, actual);
+}
+TEST(PiezasTest, pieceAtOutLTTop){
+	Piezas board;
+	Piece actual = board.pieceAt(3, 0);
+	ASSERT_EQ(Invalid, actual);
+} 
+TEST(PiezasTest, pieceAtOutRTTop){
+	Piezas board;
+	Piece actual = board.pieceAt(3, 2);
+	ASSERT_EQ(Invalid, actual);
+} 
+TEST(PiezasTest, pieceAtOutRTDiagonal){
+	Piezas board;
+	Piece actual = board.pieceAt(3, 4);
+	ASSERT_EQ(Invalid, actual);
+}
+TEST(PieceTest, pieceAtBlank1){
+	Piezas board;
+	Piece actual = board.pieceAt(0, 0);
+	ASSERT_EQ(Blank, actual);
+}
+TEST(PieceTest, pieceAtBlank2){
+	Piezas board;
+	Piece actual = board.pieceAt(0, 1);
+	ASSERT_EQ(Blank, actual);
+}
+TEST(PieceTest, pieceAtBlank3){
+	Piezas board;
+	Piece actual = board.pieceAt(0, 2);
+	ASSERT_EQ(Blank, actual);
+}
+TEST(PieceTest, pieceAtBlank4){
+	Piezas board;
+	Piece actual = board.pieceAt(0, 3);
+	ASSERT_EQ(Blank, actual);
+}
+TEST(PieceTest, pieceAtBlankTopCorner1){
+	Piezas board;
+	Piece actual = board.pieceAt(2, 0);
+	ASSERT_EQ(Blank, actual);
+}
+TEST(PieceTest, pieceAtBlankTopCorner2){
+	Piezas board;
+	Piece actual = board.pieceAt(2, 3);
+	ASSERT_EQ(Blank, actual);
+}
+TEST(PieceTest, pieceAtBlankMid1){
+	Piezas board;
+	Piece actual = board.pieceAt(1, 1);
+	ASSERT_EQ(Blank, actual);
+}
+TEST(PieceTest, pieceAtBlankMid2){
+	Piezas board;
+	Piece actual = board.pieceAt(1, 2);
+	ASSERT_EQ(Blank, actual);
+}
+TEST(PieceTest, dropPieceFilledColOError){
+	Piezas board;
+	board.dropPiece(1);
+	board.dropPiece(1);
+	board.dropPiece(1);
+	Piece actual = board.dropPiece(1);
+	ASSERT_EQ(Blank, actual);
+}
+TEST(PieceTest, dropPieceFilledColXError){
+	Piezas board;
+	board.dropPiece(1);
+	board.dropPiece(2);
+	board.dropPiece(1);
+	board.dropPiece(1);
+	Piece actual = board.dropPiece(1);
+	ASSERT_EQ(Blank, actual);
+}
+
